87682bc4f74430d5ec0ee61708874b0a0a0de1a7
Griegoriens <skwarnel@gmail.com>
2023-04-08 21:07:14 +0200

    Arrays: Task 6 - finished

---
+++ ONL_FSB_E_24_Prework_JavaScriptCOPY/02_Tablice/06_Tablice_z_Imionami/js/app.js
@@ -0,0 +1,17 @@
+const names1 = ["Jan", "Maria", "Kasia", "Wojtek", "Zosia"];
+
+console.log(names1[1]);
+console.log(names1[4]);
+console.log(names1.length);
+
+const names2 = [];
+names2.push("Jan");
+names2.push("Maria");
+names2.push("Kasia");
+names2.push("Wojtek");
+names2.push("Zosia");
+names2.push("Grzesiek");
+
+console.log(names2[0]);
+console.log(names2[2]);
+console.log(names2.length);
