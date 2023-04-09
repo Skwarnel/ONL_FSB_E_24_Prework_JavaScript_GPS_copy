7b1f4de23fa5d8fc0e18d065758ac0032c80005f
Griegoriens <skwarnel@gmail.com>
2023-04-09 11:43:59 +0200

    Arrays: Task 7 -> finished

---
+++ ONL_FSB_E_24_Prework_JavaScriptCOPY/02_Tablice/07_Tablice_2D/js/app.js
@@ -0,0 +1,18 @@
+const arrayOfNumbers = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12]
+]
+
+console.log(arrayOfNumbers[0][1]);
+console.log(arrayOfNumbers[1]);
+console.log(arrayOfNumbers[2].length);
+
+const mixedArray = [
+    ["Maria", "Jan", "Piotr"],
+    [1, 2, 3, 4, 5, 6]
+];
+
+console.log(mixedArray[0][2]);
+console.log(mixedArray[0][4]);
+console.log(mixedArray[1].length);
