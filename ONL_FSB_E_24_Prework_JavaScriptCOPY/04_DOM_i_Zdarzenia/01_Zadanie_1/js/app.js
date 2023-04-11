78fedd755525eabe70f8d118f8f2c5608878849e
Griegoriens <skwarnel@gmail.com>
2023-04-11 13:56:37 +0200

    DOM -> Task 1: finished

--- ONL_FSB_E_24_Prework_JavaScriptCOPY/04_DOM_i_Zdarzenia/01_Zadanie_1/js/app.js
+++ ONL_FSB_E_24_Prework_JavaScriptCOPY/04_DOM_i_Zdarzenia/01_Zadanie_1/js/app.js
@@ -1,1 +1,12 @@
 const childElements = document.querySelector("article").children;
+
+console.log(childElements);
+function getTags(elements) {
+    const array = [];
+    for (let i = 0; i < elements.length; i++) {
+        array.push(elements[i].localName);
+    }
+    return array;
+}
+
+console.log("Result is: " + getTags(childElements));
